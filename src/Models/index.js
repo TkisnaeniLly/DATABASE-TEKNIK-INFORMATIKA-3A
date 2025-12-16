8b86ea607438e2b433ea9cd8b1e1233f6e4f7008
ndeso17 <khilmyfirdausr171102@gmail.com>
2025-12-16 09:19:16 +0700

    feat: add Catalog

--- src/Models/index.js
+++ src/Models/index.js
@@ -3,14 +3,23 @@ const sequelize = require("../Config/sequelizeConnect");
 // ==================
 // Import Models
 // ==================
-const User = require("./scripts/User");
-const UserProfile = require("./scripts/UserProfile");
-const EmailVerification = require("./scripts/EmailVerification");
-const UserLoginDevice = require("./scripts/UserLoginDevice");
-const LoginOtp = require("./scripts/LoginOtp");
+// Auth
+const User = require("./scripts/Auth/User");
+const UserProfile = require("./scripts/Auth/UserProfile");
+const EmailVerification = require("./scripts/Auth/EmailVerification");
+const UserLoginDevice = require("./scripts/Auth/UserLoginDevice");
+const LoginOtp = require("./scripts/Auth/LoginOtp");
+
+// Catalog
+const Product = require("./scripts/Catalog/Product");
+const Category = require("./scripts/Catalog/Category");
+const Brand = require("./scripts/Catalog/Brand");
+const Media = require("./scripts/Catalog/Media");
+const Variant = require("./scripts/Catalog/Variant");
+const Inventory = require("./scripts/Catalog/Inventory");
 
 // ==================
-// Relasi
+// Relasi Auth
 // ==================
 
 // User -> Profile (1 : 1)
@@ -60,11 +69,73 @@ LoginOtp.belongsTo(UserLoginDevice, {
   targetKey: "device_id",
 });
 
+// ==================
+// Relasi Catalog
+// ==================
+
+// Category -> Product (1 : N)
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "RESTRICT",
+});
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
+
+// Brand -> Product (1 : N)
+Brand.hasMany(Product, {
+  foreignKey: "brand_id",
+  onDelete: "RESTRICT",
+});
+Product.belongsTo(Brand, {
+  foreignKey: "brand_id",
+});
+
+// Product -> Media (1 : N)
+Product.hasMany(Media, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
+Media.belongsTo(Product, {
+  foreignKey: "product_id",
+});
+
+// Product -> Variant (1 : N)
+Product.hasMany(Variant, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
+Variant.belongsTo(Product, {
+  foreignKey: "product_id",
+});
+
+// Variant -> Inventory (1 : 1)
+Variant.hasOne(Inventory, {
+  foreignKey: "variant_id",
+  onDelete: "CASCADE",
+});
+Inventory.belongsTo(Variant, {
+  foreignKey: "variant_id",
+});
+
+// ==================
+// Export
+// ==================
 module.exports = {
   sequelize,
+
+  // Auth
   User,
   UserProfile,
   EmailVerification,
   UserLoginDevice,
   LoginOtp,
+
+  // Catalog
+  Product,
+  Category,
+  Brand,
+  Media,
+  Variant,
+  Inventory,
 };
